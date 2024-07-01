d9b547f6d437872a3778536779dc379e122dfb38
leryyvpn <149588547+leryyvpn@users.noreply.github.com>
2024-07-02 01:00:46 +0700

    Update

--- update.sh
+++ update.sh
@@ -1,20 +1,51 @@
 #!/bin/bash
-dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
-biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
-###########- COLOR CODE -##############
-REPO="https://raw.githubusercontent.com/leryyvpn/super/main/"
-echo -e " [INFO] Downloading Update File"
-sleep 2
-wget ${REPO}menu/menu.zip
+clear
+fun_bar() {
+    CMD[0]="$1"
+    CMD[1]="$2"
+    (
+        [[ -e $HOME/fim ]] && rm $HOME/fim
+        ${CMD[0]} -y >/dev/null 2>&1
+        ${CMD[1]} -y >/dev/null 2>&1
+        touch $HOME/fim
+    ) >/dev/null 2>&1 &
+    tput civis
+    echo -ne "\033[0;33mPlease Wait Loading \033[1;37m- \033[0;33m["
+    while true; do
+        for ((i = 0; i < 18; i++)); do
+            echo -ne "\033[0;32m#"
+            sleep 0.1s
+        done
+        [[ -e $HOME/fim ]] && rm $HOME/fim && break
+        echo -e "\033[0;33m]"
+        sleep 1s
+        tput cuu1
+        tput dl1
+        echo -ne "\033[0;33mPlease Wait Loading \033[1;37m- \033[0;33m["
+    done
+    echo -e "\033[0;33m]\033[1;37m -\033[1;32m OK !\033[1;37m"
+    tput cnorm
+}
+res1() {
+    wget https://raw.githubusercontent.com/lerryvpn /super/main/menu/menu.zip
     unzip menu.zip
     chmod +x menu/*
     mv menu/* /usr/local/sbin
     rm -rf menu
     rm -rf menu.zip
+    rm -rf update.sh
+}
+netfilter-persistent
+clear
 
-echo -e " [INFO] Update Successfully"
-rm -rf update.sh
-sleep 2
-read -n 1 -s -r -p "Press [ Enter ] to back on menu"
+echo -e ""
+echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | lolcat
+echo -e "\e[1;97;101m            » UPDATE SCRIPT «             \033[0m"
+echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | lolcat
+echo -e ""
+echo -e "\033[1;91mUpdate Script Service\033[1;37m"
+fun_bar 'res1'
+echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | lolcat
+echo -e ""
+read -n 1 -s -r -p "Press [ Enter ] To Back On Menu"
 menu
-exit
